0f9519500346d6b4fe11c2f44108bf4937a0982a
Diana <dianadorojuc3@gmail.com>
2024-11-19 10:42:29 +0100

    Initial commit

---
+++ src/main/java/dianafriptuleac/capstone_flavor_love/CapstoneFlavorLoveApplication.java
@@ -0,0 +1,13 @@
+package dianafriptuleac.capstone_flavor_love;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneFlavorLoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneFlavorLoveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dianafriptuleac/capstone_flavor_love/CapstoneFlavorLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package dianafriptuleac.capstone_flavor_love;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneFlavorLoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
